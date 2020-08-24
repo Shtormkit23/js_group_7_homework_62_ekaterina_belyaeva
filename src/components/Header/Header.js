80ec4e629339de346c2cb73a86c21c40113b7f84
Belyaeva Ekaterina <katrinamalinashtorm@gmail.com>
2020-08-24 18:23:32 +0600

    Создан компонент Header.js и добавлены стили к нему Header.css

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Header.css'
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="Recipes-img/header-logo.png" alt="header-logo"/>
+        </header>
+    );
+};
+
+export default Header;
